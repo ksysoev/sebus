98b9820206c857a82ae392fe805baffd6194c2e4
Kirill Sysoev <k.sysoev@me.com>
2024-01-28 12:30:54 +0800

    Adds subscription publishing test

--- subscription_test.go
+++ subscription_test.go
@@ -5,6 +5,16 @@ import (
 	"testing"
 )
 
+type testEvent struct{}
+
+func (testEvent) Topic() string {
+	return "test_topic"
+}
+
+func (testEvent) Data() any {
+	return "test_data"
+}
+
 func TestNewSubscription(t *testing.T) {
 	topic := "test_topic"
 	bufferSize := uint(10)
@@ -44,5 +54,25 @@ func TestSubscription_close(t *testing.T) {
 
 	if sub.Err() != ErrTest {
 		t.Errorf("Expected error to be %v after closing, but got %v", ErrTest, sub.Err())
+	}
+}
+func TestSubscription_publish(t *testing.T) {
+	topic := "test_topic"
+	bufferSize := uint(10)
+	sub := newSubscription(topic, bufferSize)
+
+	// Publish events until buffer is full
+	for i := 0; i < int(bufferSize); i++ {
+		event := testEvent{}
+		err := sub.publish(event)
+		if err != nil {
+			t.Errorf("Expected publish to succeed, but got error: %v", err)
+		}
+	}
+
+	// Publish event when buffer is full
+	err := sub.publish(testEvent{})
+	if err != ErrSubscriptionBufferOverflow {
+		t.Errorf("Expected publish to fail with ErrSubscriptionBufferOverflow, but got error: %v", err)
 	}
 }
